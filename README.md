4f6e1b8a2e456388f726fae40ba9f93213d6edc5
Sebi Nievas <sebinievas@gmail.com>
2020-05-17 12:13:20 -0700

    Lesson 80

---
+++ README.md
@@ -0,0 +1,7 @@
+# microservices
+
+The `client-old` directory is the files I extracted I believe from an "original.zip" resource file that was downloaded.
+* I attempted to create a fresh react-app just in case it was a version issue.
+* I am able to run `npm start` from the client folder on my local machine just fine.
+* Also, note that I changed the hostname from `posts.com` to `posts.dev` as another attempt. It didn't make a difference.
+* I am able to reach with postman `posts.dev/posts/create` and create the record successfully.. this tells me the routes setup in ingress are correct, I just can't get the react client to load properly at `/`
--- posts/index.js
+++ posts/index.js
@@ -14,19 +14,19 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts/create', (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
   posts[id] = {
     id,
-    title
+    title,
   };
 
-  axios.post('http://localhost:4005/events', {
+  axios.post('http://event-bus-srv:4005/events', {
     type: 'PostCreated',
-    data: posts[id]
-  })
+    data: posts[id],
+  });
 
   res.status(201).send(posts[id]);
 });
@@ -38,5 +38,6 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
+  console.log('v55');
   console.log('Listening on 4000');
 });
